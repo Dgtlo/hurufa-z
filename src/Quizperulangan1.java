0ba5e3597bc61f76afcdfbda53d2f9ebadf2a5bb
Lenovo <Lenovo@LAPTOP-P9CTSAP4>
2023-02-02 21:32:42 +0800

    tugas

---
+++ src/Quizperulangan1.java
@@ -0,0 +1,27 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Lenovo
+ */
+public class Quizperulangan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char huruf ='z';
+        while (huruf >= 'a') 
+        {
+        System.out.print(huruf+"");
+        huruf--;
+        }
+        System.out.println("");
+    }
+    
+}
